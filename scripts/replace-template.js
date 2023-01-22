1b64249558945e94becb3b7a28e2879baa422898
Pete McKinney <pete.mckinney@semanticbits.com>
2023-01-22 01:43:41 -0600

    Added template file and replace script

---
+++ scripts/replace-template.js
@@ -0,0 +1,15 @@
+import * as fs from 'fs';
+
+const templateString = fs.readFileSync('./template/template.html');
+
+
+fs.readFile('./background.js', 'utf8', function(err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    var result = data.replace(/\`(\n|.)*?\`/, `\`${templateString}\``);
+
+    fs.writeFile('./background.js', result, 'utf8', function(err) {
+        if (err) return console.log(err);
+    });
+});
